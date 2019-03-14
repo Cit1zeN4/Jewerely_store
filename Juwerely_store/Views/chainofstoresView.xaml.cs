f2d1465aabffa3c69385706d22d6b5b573e52ec2
Tim <arthaos14@gmail.com>
2019-03-14 14:40:31 +0300

    Chainofstore and Google Map

---
+++ Juwerely_store/Views/chainofstoresView.xaml.cs
@@ -0,0 +1,36 @@
+﻿
+using Juwerely_store.Res.ChainofStorePage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Juwerely_store.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для chainofstoresView.xaml
+    /// </summary>
+    public partial class chainofstoresView : UserControl
+    {
+        public chainofstoresView()
+        {
+            InitializeComponent();
+        }
+
+        private void MapShow_Click(object sender, RoutedEventArgs e)
+        {
+            Map map = new Map();
+            map.Show();
+        }
+    }
+}
